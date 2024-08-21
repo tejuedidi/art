e92e148b3d5f24940b62c87018f6815130207853
tejuedidi <tejuedidi@gmail.com>
2024-08-20 20:50:59 -0700

    init repo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// import './App.css';
+
+export default function App() {
+    return (
+        <Router>
+            <>
+                <div>
+                    <p>Hi</p>
+                </div>
+            </>
+        </Router>
+    );
+}
