8c35f07b8d3d6f75fd6e01a196c80ef3a69952a3
tejuedidi <tejuedidi@gmail.com>
2024-08-23 08:28:31 -0700

    changed param to fit geolocation as country and query were erroring

--- backend/art/urls.py
+++ backend/art/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import art
 
 urlpatterns = [
-    path('art/', art)
+    path('art/<str:region>/', art, name='art_by_region'),
 ]
--- backend/art/views.py
+++ backend/art/views.py
@@ -1,33 +1,30 @@
 import requests
 from django.http import JsonResponse
 
-def art(request):
-    # primary image
-    # asian art 
-    #tags
-    # art w sunfowers on view at the met 
-    # url = https://collectionapi.metmuseum.org/public/collection/v1/search?q=sunflowers 
-    
+def art(request, region):
     url = 'https://collectionapi.metmuseum.org/public/collection/v1/search'
-    params = {'q': 'sunflowers', 'hasImages': 'true'}
-    response = requests.get(url, params = params)
+    params = {
+        'geoLocation': region, 
+        'q': 'flowers',          
+        'hasImages': 'true'  
+    }
+     
+    response = requests.get(url, params=params)
     data = response.json()
-    
-    objects = data.get('objectIDs', [])[:5]
+
+    objects = data.get('objectIDs', [])
     details = []
-    
+
     for obj_id in objects:
         object_url = f'https://collectionapi.metmuseum.org/public/collection/v1/objects/{obj_id}'
         object_response = requests.get(object_url).json()
-        # print(object_response)
         
         details.append({
             'title': object_response.get('title', 'No title available'),
             'artist': object_response.get('artistDisplayName', 'Unknown artist'),
             'image': object_response.get('primaryImageSmall', None),
             'date': object_response.get('objectDate', 'Date unknown'),
+            'description': object_response.get('description', 'No description available'),
         })
         
-    return JsonResponse(details, safe = False)
-         
-    
+    return JsonResponse(details, safe=False)
